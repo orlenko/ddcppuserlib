3d0730987164dbd49c8292c5c89c951f21d51e9b
Vlad Orlenko <orlenko@gmail.com>
2017-06-14 14:21:16 -0700

    version 1.0.0

---
+++ src/dummy.cpp
@@ -0,0 +1,6 @@
+#include "cpp_common/src/strtools.hpp"
+
+
+void dummy_lib_func() {
+    dd::i_will_break_you(1, 2);
+}
---
+++ src/dummy.hpp
@@ -0,0 +1,6 @@
+#ifndef DDCPPUSERLIB_DUMMY
+#define DDCPPUSERLIB_DUMMY
+
+void dummy_lib_func();
+
+#endif
